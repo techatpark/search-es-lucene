41636450c3b7fdffd12fb3cc00b59d226abcd1e9
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-03-19 22:41:03 +0800

    blue

--- upgrade.sh
+++ upgrade.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+[[ `diff nginx_blue.conf nginx.conf` ]] &&  
+   (cp nginx_blue.conf nginx.conf && echo "Blue") ||
+   (cp nginx_green.conf nginx.conf && echo "Green")
+   
 docker restart production_nginx
