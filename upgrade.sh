0048d660daab6eb4d269d48a029d13b80ffef939
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-03-19 22:45:12 +0800

    test now it works

--- upgrade.sh
+++ upgrade.sh
@@ -3,5 +3,7 @@
 [[ `diff nginx_blue.conf nginx.conf` ]] &&  
    (cp nginx_blue.conf nginx.conf && echo "Blue") ||
    (cp nginx_green.conf nginx.conf && echo "Green")
-   
-docker restart production_nginx
+
+docker exec production_nginx nginx -s reload
+
+curl http://localhost:8080/
